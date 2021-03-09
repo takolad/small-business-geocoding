2c27f07c6ff339122bff7a544dc317104223b95e
Willie <niles_nelson@me.com>
2021-03-08 21:52:46 -0600

    jfaklsdhjfglaksdhglasd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,22 @@
-import Data from "../json/data"
+mapboxgl.accessToken = 'pk.eyJ1IjoidGFrb2xhZCIsImEiOiJja2x5MWRxMG8xNG82MnVwYnp0d2RlenE0In0.B_zd2XTmTSmCPhJtOCo3Vw';
+    var map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [-98.4870, 29.4227], // starting position [lng, lat]
+    zoom: 12 // starting zoom
+    });
+    console.log(map)
+
+map.on('load', function (e) {
+    /* Add the data to your map as a layer */
+    map.addLayer({
+      "id": "locations",
+      "type": "circle",
+      /* Add a GeoJSON source containing place coordinates and information. */
+      "source": {
+        "type": "geojson",
+        "data": muhData
+      }
+    });
+  });
 
---
+++ assets/json/data.js
@@ -0,0 +1,23 @@
+
+var muhData = {
+    "type": "FeatureCollection",
+    "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-98.4870, 29.4227]
+      },
+      "properties": {
+          "name": "businessName",
+          "status": "active or inactive",
+          "address": "Alamo Street 123",
+          "city": "San Antonio",
+          "country": "United States",
+          "postalCode": "78205",
+          "permitBeginDate": "dd/mm/yy",
+          "permitEndDate": "dd/mm/yy" 
+      }
+    }
+  ]
+}
