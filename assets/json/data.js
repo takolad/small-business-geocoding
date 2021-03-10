47228fa8aa8f90c8b15f4d5e3ad9567b6fbaa109
Willie <niles_nelson@me.com>
2021-03-09 20:18:21 -0600

    ok

--- assets/json/data.js
+++ assets/json/data.js
@@ -6,17 +6,17 @@ var muhData = {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-98.4870, 29.4227]
+        "coordinates": [-98.4870, 29.4227],
       },
       "properties": {
-          "name": "businessName",
-          "status": "active or inactive",
-          "address": "Alamo Street 123",
-          "city": "San Antonio",
-          "country": "United States",
-          "postalCode": "78205",
-          "permitBeginDate": "dd/mm/yy",
-          "permitEndDate": "dd/mm/yy" 
+      "name": "businessName",
+      "status": "active or inactive",
+      "address": "Alamo Street 123",
+      "city": "San Antonio",
+      "country": "United States",
+      "postalCode": "78205",
+      "permitBeginDate": "dd/mm/yy",
+      "permitEndDate": "dd/mm/yy" 
       }
     }
   ]
